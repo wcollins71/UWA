d3bc17b0d0491d2eb7910f7e89ff1daf22f7f78d
Wayne Collins <wayne.c@tcwa.com.au>
2020-09-26 15:58:30 +0800

    working copy

--- 05-Third-Party-APIs/Assets/script.js
+++ 05-Third-Party-APIs/Assets/script.js
@@ -3,64 +3,65 @@ function addScheduleRow(times) {
     const row = $("<div>");
 
     row.attr("class", "row");
-    row.attr("id", times);
-
-    // in the row
-    // has 3 cols
-
-    // first col we should insert time
+    //  row.attr("id", times);
 
     const timeCol = $("<a>");
     timeCol.attr("class", "col-sm-2 time-block hour");
-    timeCol.text(times);
+    const timeToDisplay = moment(times, "ha");
+    const timeToDisplayHa = timeToDisplay.format("ha");
+    timeCol.text(timeToDisplayHa);
     row.append(timeCol);
 
     const textCol = $("<textarea>");
     textCol.attr("class", "col-sm-9");
-
-    //   const timenow = 1600927200000;
-    const timenow = moment();
-    const timeNowHa = timenow.format("ha");
-
-    const timeMoment = moment(times, "ha");
-    const timeMomentHa = timeMoment.format("ha");
-
-    console.log("timenow " + timeNowHa);
-    console.log("timeMoment " + timeMomentHa);
-
-    if (timeMoment < timenow) {
-        console.log("Past");
+    textCol.attr("id", "text-" + times)
+    textCol.text(localStorage.getItem(times));
+    const timenow = moment().format("HH");
+    const timeMoment = moment(times, "HH");
+    const timeMomentHa = timeMoment.format("HH");
+    if (timenow > timeMomentHa) {
         textCol.addClass("past");
-    }
-    else if (timeMoment === timenow) {
-        console.log("present");
+    } else if (timenow == timeMomentHa) {
         textCol.addClass("present");
-    } else {
-        console.log("future");
+    } else if (timenow < timeMomentHa) {
         textCol.addClass("future");
+    } else {
+        console.log("error");
     }
-    row.attr("id", timeMomentHa)
+
+    console.log("text-" + times)
     row.append(textCol);
 
-    const buttonCol = $("<div>");
+    const buttonCol = $("<button>");
     buttonCol.attr("class", "col-sm-1 saveBtn");
-    row.append(buttonCol);
+    buttonCol.attr("id", times);
 
     const button = $("<i>");
-    button.attr("class", "fas fa-save");
+    button.attr("class", "fas fa-save fa-lg");
+    //   button.css("padding", "20px");
+    //  button.attr("id", "buttonSave" + times);
     buttonCol.append(button);
-
+    row.append(buttonCol);
     $(".container").append(row);
 
 }
 
 $(document).ready(function () {
-    const times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+    const times = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
     const timenow = moment();
     for (let index = 0; index < times.length; index++) {
-
         addScheduleRow(times[index]);
-
     }
 
+    $(".saveBtn").on("click", function (event) {
+        event.preventDefault();
+        var buttonClicked = $(this).attr("id");
+        console.log("Click " + buttonClicked);
+        var textToSave = $("#text-" + buttonClicked).val();
+        console.log("textToSave " + textToSave);
+        localStorage.setItem(buttonClicked, textToSave);
+    })
+
+
 });
+
